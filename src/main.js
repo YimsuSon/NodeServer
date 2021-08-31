de6bc40a007eff5e69083fb70c9fc5a01cf8e875
yimsuson <brushhouse59@gmail.com>
2021-08-31 15:21:44 +0900

    Making server without framework

---
+++ src/main.js
@@ -0,0 +1,102 @@
+//@ts-check
+// Formatting, Linting, Type Checking
+// Formatting: Prettier - 코드를 짤때 피드백을 주기때문에 청결한 코드관리가 가능
+// Linting: ESLint
+// Type checing : TypeScript
+
+// 프레임워크 없이 간단한 토이프로젝트 웹 서버 만들어보기 - 블로그 포스팅 서비스 , 로컬 파일을 데이터베이스로 활용(JSON), 인증로직 x, Restful API
+
+const http = require('http')
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'Gi',
+  },
+  {
+    id: 'my_second_post',
+    title: '나의 두번째 호스트',
+    content: 'Second post',
+  },
+]
+
+/**
+ * Post
+ *
+ * GET /posts
+ * GET /posts/:id
+ * POST /posts
+ *
+ */
+
+const server = http.createServer((req, res) => {
+  const POST_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
+  const postIDRegexResult =
+    (req.url && POST_ID_REGEX.exec(req.url)) || undefined
+
+  if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    }
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json; encoding=utf-8')
+    res.end(JSON.stringify(result))
+  } else if (postIDRegexResult && req.method === 'GET') {
+    // GET /posts/:id
+    const postId = postIDRegexResult[1]
+    const post = posts.find((_post) => _post.id === postId)
+
+    if (post) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json; encoding=utf-8')
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Post Not found')
+    }
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8')
+    req.on('data', (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       * */
+
+      /** @type {CreatePostBody} */
+      const body = JSON.parse(data)
+      console.log(body)
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, '_'),
+        title: body.title,
+        content: body.content,
+      })
+    })
+
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not found')
+  }
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log(`The server is listening : ${PORT}`)
+})
